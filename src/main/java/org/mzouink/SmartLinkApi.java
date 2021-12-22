4268e7c56283108bb9633ddc3136df46a9c70063
mzouink <zouinkhi.marwan@gmail.com>
2021-12-22 15:58:04 -0500

    add filter time

--- src/main/java/org/mzouink/SmartLinkApi.java
+++ src/main/java/org/mzouink/SmartLinkApi.java
@@ -20,20 +20,20 @@ public class SmartLinkApi {
     public static Map<String, SmartLink> currentLinks = new HashMap<>();
     public static Map<String, HistorySmartLink> historyLinks = new HashMap<>();
 
+    private final static long BEFORE_TIME = 1000 * 60 * 60; // An hour
+
     @GET
     @Path("get")
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.TEXT_PLAIN)
-    public Response get(@QueryParam("id") String id){
-        if(currentLinks.containsKey(id)){
+    public Response get(@QueryParam("id") String id) {
+        if (currentLinks.containsKey(id)) {
             SmartLink link = currentLinks.get(id);
-        historyLinks.put(id,new HistorySmartLink(link));
-        currentLinks.remove(id);
-        return Response.ok(link.getJson()).build();
-        }
-        else{
+            historyLinks.put(id, new HistorySmartLink(link));
+            currentLinks.remove(id);
+            return Response.ok(link.getJson()).build();
+        } else
             return Response.status(Response.Status.BAD_REQUEST).build();
-        }
     }
 
 
@@ -41,27 +41,39 @@ public class SmartLinkApi {
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.TEXT_PLAIN)
     @Path("create")
-    public Response create(String json){
+    public Response create(String json) {
+
         String id = SmartLink.generateID().toUpperCase();
-        while(currentLinks.containsKey(id)){
+        while (currentLinks.containsKey(id)) {
             id = SmartLink.generateID();
         }
-        SmartLink link  = new SmartLink(id,json);
-        currentLinks.put(id,link);
+        SmartLink link = new SmartLink(id, json);
+        currentLinks.put(id, link);
+        clean();
         return Response.ok(id).build();
+    }
+
+    private void clean() {
+        long timeFilter = System.currentTimeMillis() - BEFORE_TIME;
+        new Thread(() -> {
+            for (SmartLink sm : currentLinks.values()) {
+                if (Long.parseLong(sm.getTs()) < timeFilter)
+                    currentLinks.remove(sm.getID());
+            }
+        }).start();
     }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("test")
-    public Response getAll(){
+    public Response getAll() {
         return Response.ok(currentLinks.values()).build();
     }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("history")
-    public Response getHistory(){
+    public Response getHistory() {
         return Response.ok(historyLinks.values()).build();
     }
 }
